21d31af3b70dc7e4dbdc2f8eda8d871093e6dabc
murarivamsharaj <chmurari73@gmail.com>
2026-04-12 21:14:22 +0530

    feat: new changes

---
+++ java-primitives/src/com/java/primitives/ConditionalStatmentsTest.java
@@ -0,0 +1,12 @@
+package com.java.primitives;
+
+public class ConditionalStatmentsTest {
+    static void main() {
+        int i = -10;
+        if (i > 0) {
+            System.out.println("The given no. is positive");
+        } else  {
+            System.out.println("the given no. is negative");
+        }
+    }
+}
