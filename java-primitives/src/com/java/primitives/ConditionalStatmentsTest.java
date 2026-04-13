dcbfa27d89c92a3a652d15e8a7a24136b89df4cd
murarivamsharaj <chmurari73@gmail.com>
2026-04-13 08:49:18 +0530

    feat: new  changes

--- java-primitives/src/com/java/primitives/ConditionalStatmentsTest.java
+++ java-primitives/src/com/java/primitives/ConditionalStatmentsTest.java
@@ -2,13 +2,12 @@ package com.java.primitives;
 
 public class ConditionalStatmentsTest {
     static void main() {
-        int i = 100;
-        if (i >= 200) {
-            System.out.println("The given no. is positive");
-        } else if (i <= 185) {
-            System.out.println("no. is negative");
-        } else  {
-            System.out.println("invalid no.");
+        int evenOdd = 100;
+
+        if ( evenOdd % 2 == 0) {
+            System.out.println("Print even ");
+        } else {
+            System.out.println("Print odd");
         }
     }
 }
