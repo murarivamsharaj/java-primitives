0da7d4a91a47f8e46749ca1470a90e8d18ee6065
murarivamsharaj <chmurari73@gmail.com>
2026-04-13 15:06:11 +0530

    feat: new changes

--- java-primitives/src/com/java/primitives/ConditionalStatmentsTest.java
+++ java-primitives/src/com/java/primitives/ConditionalStatmentsTest.java
@@ -1,13 +1,22 @@
 package com.java.primitives;
 
+import java.util.Scanner;
+
 public class ConditionalStatmentsTest {
     static void main() {
-        int evenOdd = 100;
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter two  numbers");
+        int evenOdd = Sc.nextInt();
+        int oddEven = Sc.nextInt();
 
-        if ( evenOdd % 2 == 0) {
-            System.out.println("Print even ");
+
+
+        if (evenOdd > oddEven) {
+            System.out.println("print even greater");
+        } else if ( evenOdd < oddEven) {
+            System.out.println("odd is greater ");
         } else {
-            System.out.println("Print odd");
+            System.out.println(" Invalid no.");
         }
     }
 }
